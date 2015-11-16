2eeba0dfcbba3f5e987dfeddc98ff390dfc0e28d
charroux <benoit.charroux@free.fr>
2015-11-16 17:06:21 +0100

    adding cargo to buid.gradle

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -9,6 +9,14 @@ app.config(['$routeProvider',
 			when('/location', {
 				templateUrl: 'partials/menu.html'
 			}).
+			when('/location/creerCar', {
+				templateUrl: 'partials/creerCar.html',
+				controller: 'creerCarController'
+			}).
+			when('/location/supprimerCar', {
+				templateUrl: 'partials/supprimerCar.html',
+				controller: 'supprimerCarController'
+			}).
 			when('/location/louer', {
 				templateUrl: 'partials/louer.html',
 				controller: 'louerController'
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,6 +2,35 @@
 
 var rentControllers = angular.module('rentControllers', []);
 
+rentControllers.controller("creerCarController", function($scope,$http,$routeParams) {
+
+	$scope.createCar = function() {
+		$http.post('http://localhost:8080/RestWebService/car/AZE', 'AAA').
+		  success(function(data, status, headers, config) {
+		  }).
+		  error(function(data, status, headers, config) {
+			  alert("Error: " + status + " while accessing $http.post('http://localhost:8080/RestWebService/car/')");
+		  });
+	};
+	        
+});
+
+rentControllers.controller("supprimerCarController", function($scope,$http,$routeParams) {
+
+	$scope.plateNumber = "";
+	
+	$scope.supprimerCar = function(plateNumber) {
+		$http.delete('http://localhost:8080/RestWebService/car/'+plateNumber).
+		  success(function(data, status, headers, config) {
+		  }).
+		  error(function(data, status, headers, config) {
+			  alert("Error: " + status + " while accessing $http.delete('http://localhost:8080/RestWebService/car/'+plateNumber)");
+		  });
+	};
+	        
+});
+		
+		
 rentControllers.controller("louerController", function($scope,$http,$routeParams) {
 
 $http.get('http://localhost:8080/RestWebService/car').
@@ -14,11 +43,11 @@ $http.get('http://localhost:8080/RestWebService/car').
   });
 
 $scope.rent = function(car) {
-	$http.delete('http://localhost:8080/RestWebService/car/'+car.plateNumber).
+	$http.put('http://localhost:8080/RestWebService/car/'+car.plateNumber+'?rent=true').
 	  success(function(data, status, headers, config) {
 	  }).
 	  error(function(data, status, headers, config) {
-		  alert("Error: " + status + " while accessing $http.delete('http://localhost:8080/RestWebService/car/'+car.plateNumber)");
+		  alert("Error: " + status + " while accessing $http.put('http://localhost:8080/RestWebService/car/'+car.plateNumber)");
 	  });
 };
         
@@ -29,7 +58,7 @@ rentControllers.controller("ramenerController", function($scope,$http,$routePara
 	$scope.plateNumber = "";
 	
 	$scope.getBack = function(plateNumber) {
-		$http.put('http://localhost:8080/RestWebService/car/'+plateNumber).
+		$http.put('http://localhost:8080/RestWebService/car/'+plateNumber+'?rent=false').
 		  success(function(data, status, headers, config) {
 		  }).
 		  error(function(data, status, headers, config) {
@@ -37,4 +66,4 @@ rentControllers.controller("ramenerController", function($scope,$http,$routePara
 		  });
 	};
 	        
-	});
+});
